bc3c98b5782a74491d57892a964df3dd528c2515
jyccheun <jasoncheung0909@gmail.com>
2022-11-01 16:48:24 -0400

    update

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
 import { Pedometer } from 'expo-sensors';
 import { request, PERMISSIONS } from 'react-native-permissions';
 
 export default function App() {
   const [currentStepCount, setCurrentStepCount] = useState(0)
+  const subscriptionRef = useRef()
 
   useEffect(() => {
-    const subscription = Pedometer.watchStepCount(result => setCurrentStepCount(result.steps))
+    subscriptionRef.current = Pedometer.watchStepCount(result => setCurrentStepCount(result.steps))
 
     if (Platform.OS === 'android') {
       request(PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION)
@@ -16,13 +17,20 @@ export default function App() {
     }
 
     return () => {
-      subscription.remove()
+      subscriptionRef.current.remove()
     }
   }, [])
+
+  const reset = () => {
+    setCurrentStepCount(0)
+    subscriptionRef.current.remove()
+    subscriptionRef.current = Pedometer.watchStepCount(result => setCurrentStepCount(result.steps))
+  }
 
   return (
     <View style={styles.container}>
       <Text>Walk! And watch this go up: {currentStepCount}</Text>
+      <Button onPress={reset} title="Reset"/>
     </View>
   );
 }
